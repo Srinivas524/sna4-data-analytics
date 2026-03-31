1e73c2521bf68428e3c9348478c1bc39b43e17d1
Srinivas524 <1srinivas.j@gmail.com>
2026-03-30 23:32:34 -0700

    downgrade 3.7 to 3.6

--- sna4.user.js
+++ sna4.user.js
@@ -1,9 +1,13 @@
 // ==UserScript==
 // @name         SNA4 Data Analytics — Bootloader
 // @namespace    http://tampermonkey.net/
-// @version      3.7
+// @version      3.5
 // @description  Thin bootloader stub — loads private config from SharePoint
-// @match        https://amazon.sharepoint.com/sites/TackAnalysis/SitePages/*
+// @match        https://amazon.sharepoint.com/sites/TackAnalysis/SitePages/Home.aspx
+// @match        https://amazon.sharepoint.com/sites/TackAnalysis/SitePages/TaktTimeStudy.aspx
+// @match        https://amazon.sharepoint.com/sites/TackAnalysis/SitePages/InferredAnalysis.aspx
+// @match        https://amazon.sharepoint.com/sites/TackAnalysis/SitePages/CollabHome.aspx
+// @match        https://amazon.sharepoint.com/sites/TackAnalysis/SitePages/OB-Planner.aspx
 // @match        https://iad.alps-basecamp.lamps.amazon.dev/SNA4/*
 // @match        http://connrand-dev.aka.corp.amazon.com:3000/SNA4/kiosk/facilities/pit_red_tags*
 // @run-at       document-start
@@ -22,7 +26,6 @@
 // @connect      alps-iad.iad.proxy.amazon.com
 // @connect      prod-pdx.yinliy.people.amazon.dev
 // @connect      sspot.iad.corp.amazon.com
-// @connect      foresight.amazon.com
 // @updateURL    https://raw.githubusercontent.com/Srinivas524/sna4-data-analytics/main/sna4.user.js
 // @downloadURL  https://raw.githubusercontent.com/Srinivas524/sna4-data-analytics/main/sna4.user.js
 // ==/UserScript==
